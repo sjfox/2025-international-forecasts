b76889087aa069b75a39ec103cd513d8c88f38be
MayaSal <mariah.salcedo@uga.edu>
2025-06-23 10:17:17 -0400

    Update INLA files for new week dropping schema

--- R/run-INFLAenza.R
+++ R/run-INFLAenza.R
@@ -9,8 +9,19 @@ inla.setOption(inla.mode="classic")
 ###############
 #Define Parameters 
 ###############
+country_parm_table <- tibble(
+  country           = c('Australia','Brazil','Chile','South Africa','Thailand'),
+  horizons_dropped  = c(1,           3,       2,      2,             0),
+  min_fcast_horizon = c(-1,          -1,      -1,     -1,            -1),
+  max_fcast_horizon = c(3,           3,       3,      3,             3)
+)
 
-horizon_neg_1 <- earliest_expected_data_date
+horizon_0 <- forecast_date + days(3)
+horizon_neg_1 <- forecast_date - days(4) 
+
+global_min <- min(country_parm_table$min_fcast_horizon)
+global_max <- max(country_parm_table$max_fcast_horizon)
+forecast_horizon_global <- global_max - global_min + 1
 
 #############
 #Read in and clean data 
@@ -46,6 +57,11 @@ df_others <- df %>%
 final_df <- bind_rows(df_Brazil_Chile, df_others) %>%
   arrange(COUNTRY, ISO_SDATE)
 
+latest_dates <- final_df %>%
+  group_by(COUNTRY) %>%
+  summarise(latest_date = max(ISO_SDATE, na.rm = TRUE)) %>%
+  ungroup()
+
 #################
 #Run Models
 #################
@@ -53,25 +69,33 @@ final_df <- bind_rows(df_Brazil_Chile, df_others) %>%
 ########### #define model/fit data/ fit/ sample/ summarize
 model <- model_formula("iid", temporal="ar1", spatial="iid") #normal
 
-fit_df <- final_df |> 
-  filter(ISO_SDATE < horizon_neg_1) |> 
-  prep_fit_data(forecast_horizon) 
-
-fit_df <- drop_weeks(fit_df, weeks_to_drop, date_col = "ISO_SDATE")
+fit_df <- final_df %>%
+  left_join(country_parm_table,
+            by = c("COUNTRY" = "country")) %>%
+  filter(ISO_SDATE < (horizon_0 - weeks(horizons_dropped))) %>%
+  prep_fit_data(forecast_horizon_global)
   
 fit <- fit_inla_model(fit_df, horizon_neg_1, model, pc_prior_u=c(1, 1))
 
 pred_summ <- sample_count_predictions(fit_df, fit, forecast_date = horizon_neg_1, nsamp=5000) |>
   summarize_quantiles()
 
-result <- hub_formatting(pred_summ)
-
-result <- result |>
-  mutate(horizon = as.integer(horizon - 1),
+result <- hub_formatting(pred_summ) %>%
+  mutate(horizon        = as.integer(horizon - 1),
          reference_date = reference_date + days(7))
+
+# 8) Now join back your table and prune country‐by‐country
+result_final <- result %>%
+  left_join(country_parm_table %>% 
+              select(country, min_fcast_horizon, max_fcast_horizon),
+            by = "country") %>%
+  filter(horizon >= min_fcast_horizon,
+         horizon <= max_fcast_horizon) %>%
+  select(-min_fcast_horizon, -max_fcast_horizon)
+
 
 ########### # Save files 
 
 result |> 
-  write_csv(paste0("processed-data/rt-forecasts/", forecast_date + days(3), "-UGA_flucast-INFLAenza.csv"))
+  write_csv(paste0("processed-data/rt-forecasts/", horizon_0, "-UGA_flucast-INFLAenza.csv"))
 
